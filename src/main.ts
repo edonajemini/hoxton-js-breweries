64923f6a3090c5259de61bc489418d40b66bcef4
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-07-06 00:33:06 +0200

    Third commit

--- src/main.ts
+++ src/main.ts
@@ -5,74 +5,170 @@
 //  Connect to a fetch
 //  Profit
 
-let state = {
-    USState: '',
-    breweries: []
+type Brewery = {
+  address_2: null;
+  address_3: null;
+  brewery_type: string;
+  city: string;
+  country: string;
+  county_province: null;
+  created_at: string;
+  id: number;
+  latitude: string;
+  longitude: string;
+  name: string;
+  obdb_id: string;
+  phone: string;
+  postal_code: string;
+  state: string;
+  street: string;
+  updated_at: string;
+  website_url: string;
+}
+
+
+const breweries: Brewery[] = [
+{
+address_2: null,
+address_3: null,
+brewery_type: 'large',
+city: 'San Diego',
+country: 'United States',
+county_province: null,
+created_at: '2018-07-24T00:00:00.000Z',
+id: 8041,
+latitude: '32.714813',
+longitude: '-117.129593',
+name: '10 Barrel Brewing Co',
+obdb_id: '10-barrel-brewing-co-san-diego',
+phone: '6195782311',
+postal_code: '92101-6618',
+state: 'California',
+street: '1501 E St',
+updated_at: '2018-08-23T00:00:00.000Z',
+website_url: 'http://10barrel.com'
+}
+]
+
+type State = {
+USState: string,
+breweries: Brewery[]
+}
+
+let state: State = {
+  USState: '',
+  breweries: []
+}
+
+let mainEl = document.querySelector('main')
+
+function renderSearch(){
+
+  let h1El = document.createElement('h1')
+  h1El.textContent = 'List of Breweries'
+
+  let searchHeader = document.createElement('header')
+  searchHeader.classList.add('search')
+
+  let searchForm = document.createElement('form')
+  searchForm.id = 'search-breweries-form'
+  searchForm.autocomplete = 'off'
+
+  let searchLabel = document.createElement('label')
+  searchLabel.htmlFor = 'search-breweries'
+  searchLabel.textContent = 'Search breweries:'
+
+  let searchInput = document.createElement('input')
+  searchInput.id = 'search-breweries'
+  searchInput.name = 'search-breweries'
+  searchInput.type = 'text'
+
+  searchForm.append(searchLabel, searchInput)
+  searchHeader.append(searchForm)
+  mainEl?.append(h1El ,searchHeader)
+
+}
+
+
+function renderListOfBrewery(){
+let articleEl = document.createElement('article')
+
+let ulEl = document.createElement('ul')
+ulEl.classList.add('breweries-list')
+
+fetch(`https://api.openbrewerydb.org/breweries?by_state=${state.USState}`, {
+  method: 'GET',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+}).then(response => response.json())
+.then(data => {
+  data.forEach((brewery: Brewery) => {
+      let liEl = document.createElement('li')
+
+
+      let h2El = document.createElement('h2')
+      h2El.textContent = brewery.name
+      let typeEl = document.createElement('div')
+      typeEl.classList.add('type')
+      typeEl.textContent = brewery.brewery_type
+      let addressEl = document.createElement('section')
+      addressEl.classList.add('address')
+      let h3El = document.createElement('h3')
+      h3El.textContent = 'Address:'
+      let pEl = document.createElement('p')
+      pEl.textContent = brewery.street
+      let p2El = document.createElement('p')
+      p2El.textContent = `${brewery.city}, ${brewery.state} ${brewery.postal_code}`
+      let phoneEl = document.createElement('section')
+      phoneEl.classList.add('phone')
+      let h3El2 = document.createElement('h3')
+      h3El2.textContent = 'Phone:'
+      let pEl2 = document.createElement('p')
+      pEl2.textContent = brewery.phone
+      let linkEl = document.createElement('section')
+      linkEl.classList.add('link')
+      let aEl = document.createElement('a')
+      aEl.href = brewery.website_url
+      aEl.textContent = 'Visit Website'
+      addressEl.append(h3El, pEl, p2El)
+      phoneEl.append(h3El2, pEl2)
+      linkEl.append(aEl)
+      liEl.append(h2El, typeEl, addressEl, phoneEl, linkEl)
+      ulEl.append(liEl)
+  })
+})
+articleEl.append(ulEl)
+mainEl?.append(articleEl)
+}
+
+function getBreweriesForState () {
+let filteredBreweries= state.breweries.filter(brewery => {
+  return brewery.state === state.USState
+})
+state.breweries = filteredBreweries
+render()
+}
+function getBreweries() {
   }
+
+function listenToSelectStateForm () {
+let formEl = document.querySelector<HTMLFormElement>('#select-state-form')
+formEl?.addEventListener('submit', function (event) {
+  event.preventDefault()
+  let USState = formEl['select-state'].value
+  state.USState = USState
+  getBreweriesForState()
+})
+}
+
+function render(){
+if(mainEl)
+mainEl.innerHTML = ""
   
-  // Q: Which state are we looking for? state.USState
-  // Q: What breweries do we need to display? state.breweries
-  
-  function getBreweriesForState () {
-    // find breweries in this state
-    // put them in state
-    // rerender
-  }
-  
-  function renderHeader () {
-    // <h1>List of Breweries</h1>
-    // <header class="search-bar">
-    //   <form id="search-breweries-form" autocomplete="off">
-    //     <label for="search-breweries"><h2>Search breweries:</h2></label>
-    //     <input id="search-breweries" name="search-breweries" type="text" />
-    //   </form>
-    // </header>
-  }
-  
-  function renderBreweryList () {
-    // <article>
-    //   <ul class="breweries-list">
-    //   </ul>
-    // </article>
-  }
-  
-  function renderSingleBrewery () {
-    //     <li>
-    //       <h2>Snow Belt Brew</h2>
-    //       <div class="type">micro</div>
-    //       <section class="address">
-    //         <h3>Address:</h3>
-    //         <p>9511 Kile Rd</p>
-    //         <p><strong>Chardon, 44024</strong></p>
-    //       </section>
-    //       <section class="phone">
-    //         <h3>Phone:</h3>
-    //         <p>N/A</p>
-    //       </section>
-    //       <section class="link">
-    //         <a href="null" target="_blank">Visit Website</a>
-    //       </section>
-    //     </li>
-  }
-  
-  function render () {
-    let mainEl = document.querySelector('main')
-    if (mainEl === null) return
-    mainEl.textContent = ''
-  
-    renderHeader()
-    renderBreweryList()
-  }
-  
-  function listenToSelectStateForm () {
-    let formEl = document.querySelector<HTMLFormElement>('#select-state-form')
-    formEl?.addEventListener('submit', function (event) {
-      event.preventDefault()
-      let USState = formEl['select-state'].value
-      state.USState = USState
-      getBreweriesForState()
-    })
-  }
-  
-  listenToSelectStateForm()
-  
+renderSearch()
+renderListOfBrewery()
+}
+
+listenToSelectStateForm()
+render()
